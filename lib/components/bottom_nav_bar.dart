e6b72eb0d2d5d4adf0018c9f387b7690a830a985
adnanahmed504423@gmail.comgit config --global user.email adnanahmed504423@gmail.com <adnanahmed504423@gmail.com>
2025-02-12 00:09:25 +0530

    commit

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -1,56 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
-class MyButtonNavBar extends StatefulWidget {
-  @override
-  _MyButtonNavBarState createState() => _MyButtonNavBarState();
-}
+class MyButtonNavBar extends StatelessWidget {
+  MyButtonNavBar({Key? key}) : super(key: key);
 
-class _MyButtonNavBarState extends State<MyButtonNavBar> {
-  int _selectedIndex = 0;
+  final ValueNotifier<int> _selectedIndex = ValueNotifier<int>(0);
 
   static const List<Widget> _widgetOptions = <Widget>[
-    Text('Home Page'),
-    Text('Cart'),
+    Center(child: Text('Home Page', style: TextStyle(fontSize: 24))),
+    Center(child: Text('Cart', style: TextStyle(fontSize: 24))),
   ];
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: _widgetOptions.elementAt(_selectedIndex),
+        body: ValueListenableBuilder<int>(
+          valueListenable: _selectedIndex,
+          builder: (context, index, child) {
+            return _widgetOptions[index];
+          },
         ),
-        bottomNavigationBar: Container(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
-            child: GNav(
-              rippleColor: Colors.grey[300]!,
-              hoverColor: Colors.grey[100]!,
-              gap: 8,
-              activeColor: Colors.black,
-              iconSize: 24,
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-              duration: Duration(milliseconds: 400),
-              tabBackgroundColor: Colors.grey[100]!,
-              color: Colors.black,
-              tabs: [
-                GButton(
-                  icon: Icons.home,
-                  text: 'home',
-                ),
-                GButton(
-                  icon: Icons.shopping_bag_outlined,
-                  text: 'Cart',
-                ),
-              ],
-              selectedIndex: _selectedIndex,
-              onTabChange: (index) {
-                setState(() {
-                  _selectedIndex = index;
-                });
-              },
-            ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+          child: GNav(
+            rippleColor: Colors.grey[300]!,
+            hoverColor: Colors.grey[100]!,
+            gap: 8,
+            activeColor: Colors.black,
+            iconSize: 24,
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+            duration: Duration(milliseconds: 400),
+            tabBackgroundColor: Colors.grey[100]!,
+            color: Colors.black,
+            selectedIndex: _selectedIndex.value, // Binding selected index
+            onTabChange: (index) {
+              _selectedIndex.value = index; // Updating the selected index
+            },
+            tabs: [
+              GButton(
+                icon: Icons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: Icons.shopping_bag_outlined,
+                text: 'Cart',
+              ),
+            ],
           ),
         ),
       ),
